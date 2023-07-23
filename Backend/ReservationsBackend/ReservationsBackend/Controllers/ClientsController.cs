729ffe9c3bb897619b95b0a37834f89b6796fa89
Viviana Liang <viviana.liang@uhispano.ac.cr>
2023-07-22 22:49:34 -0600

    creacion de CRUD clientes y controller Hotel

--- Backend/ReservationsBackend/ReservationsBackend/Controllers/ClientsController.cs
+++ Backend/ReservationsBackend/ReservationsBackend/Controllers/ClientsController.cs
@@ -7,5 +7,62 @@ namespace ReservationsBackend.Controllers
     [ApiController]
     public class ClientsController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public ClientsController(DataContext context) 
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        
+       /*public async Task<ActionResult<List<Client>>> GetAllClients(string query = "") 
+        {
+            // Searching
+            var result = await _context.Clients
+                .Where(client =>
+                    client.Id.ToString().Contains(query))
+            .ToListAsync();
+
+            return Ok(result);
+        }*/
+        
+        
+        [HttpPost]
+        public async Task<ActionResult<List<Client>>> CreateClient(Client client)
+        {
+            _context.Clients.Add(client);
+            await _context.SaveChangesAsync();
+            return Ok(client);
+        }
+
+        [HttpPut]
+        //Check this one
+        public async Task<ActionResult<List<Client>>> UpdateClient(Client updatedClient)
+        {
+            var client = await _context.Clients
+                .Include(client => client.FirstName)
+                .Include(client => client.LastName)
+                .FirstOrDefaultAsync(c => c.Id == updatedClient.Id);
+            if (client is null)
+                return NotFound("Client not found");
+            client.Phone = updatedClient.Phone;
+            client.Email = updatedClient.Email;
+            await _context.SaveChangesAsync();
+            return Ok(client);
+        }
+
+        
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Client>>> DeleteClient(int id) 
+        {
+            var client = await _context.Clients.FindAsync(id);
+            if (client is null)
+                return NotFound("Client not found. ");
+            _context.Clients.Remove(client);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
     }
 }
---
+++ Backend/ReservationsBackend/ReservationsBackend/Controllers/HotelsController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReservationsBackend.Data;
+using ReservationsBackend.Models;
+
+namespace ReservationsBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public HotelsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Hotels
+        [HttpGet]
+        public async Task<ActionResult<List<Hotel>>> GetHotels()
+        {
+          if (_context.Hotels == null)
+          {
+              return NotFound();
+          }
+            return await _context.Hotels.ToListAsync();
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Hotel>> GetSingleHotel(int id)
+        {
+          if (_context.Hotels == null)
+          {
+              return NotFound();
+          }
+            var hotel = await _context.Hotels
+                .Include(hotel => hotel.Name)
+                .Include(hotel => hotel.Description)
+                .Include(hotel => hotel.Address)
+                .Include(hotel => hotel.PostalCode)
+                //.Include(hotel => hotel.Email)
+                .FirstOrDefaultAsync(hotel => hotel.Id == id);
+
+            if (hotel is null)
+            {
+                return NotFound("Hotel not found. ");
+            return Ok(hotel);
+            }
+
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Hotel>>> UpdateHotel(Hotel updatedHotel)
+        {
+
+            var hotel = await _context.Hotels
+                .Include(Hotel => Hotel.Description)
+                .Include(Hotel => Hotel.Address)
+                .Include(Hotel => Hotel.Name)
+                .FirstOrDefaultAsync(h => h.Id == updatedHotel.Id);
+            if (hotel is null)
+                return NotFound("Hotel not found.");
+            hotel.Phone = updatedHotel.Phone;
+            hotel.Status = updatedHotel.Status;
+            await _context.SaveChangesAsync();
+            return Ok(hotel);
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Hotel>> CreateHotel(Hotel hotel)
+        {
+            _context.Hotels.Add(hotel);
+            await _context.SaveChangesAsync();
+            return Ok(hotel);
+        }
+
+        // DELETE:
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteHotel(int id)
+        {
+            var hotel = await _context.Hotels.FindAsync(id);
+            if (hotel == null)
+            
+                return NotFound("Hotel not found. ");
+                _context.Hotels.Remove(hotel);
+                await _context.SaveChangesAsync();
+                return NoContent();
+        }
+
+        private bool HotelExists(int id)
+        {
+            return (_context.Hotels?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}

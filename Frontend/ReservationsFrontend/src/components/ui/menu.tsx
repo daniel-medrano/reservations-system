21fbad3d4be8c77e59ec00964200809feef22e7a
EITHAM RODA ALFARO <eitham.r.a@hotmail.com>
2023-08-07 17:18:25 -0600

    update css

--- Frontend/ReservationsFrontend/src/components/ui/menu.tsx
+++ Frontend/ReservationsFrontend/src/components/ui/menu.tsx
@@ -2,6 +2,7 @@ import {Link} from "react-router-dom"
 
 export function Menu(){
     return (
+        <body>
         <nav className="menu">
             <Link to="/"> Home </Link>
             <Link to="/about"> About </Link>
@@ -9,5 +10,6 @@ export function Menu(){
             <Link to="/Log in"> Log in </Link>
             
         </nav>
+        </body>
     );
 }

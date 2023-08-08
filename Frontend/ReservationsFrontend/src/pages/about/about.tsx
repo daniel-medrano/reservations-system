8e30f4f06702aaca3ef3562b14ef73fe277cf658
EITHAM RODA ALFARO <eitham.r.a@hotmail.com>
2023-08-07 21:25:38 -0600

    Date picker tests

--- Frontend/ReservationsFrontend/src/pages/about/about.tsx
+++ Frontend/ReservationsFrontend/src/pages/about/about.tsx
@@ -1,11 +1,14 @@
 
 import { Button } from "@/components/ui/button"
-
+import { DatePickerWithRange } from "@/components/date-picker-range"
+ 
 export function About (){ 
     return  (
     <>
     <h1> About </h1>
     <Button> Hola </Button>
+
+    <DatePickerWithRange />
     </>
     )
 }

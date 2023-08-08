74cacfbf5726f51735b313695a6bfa5bf5b1e251
EITHAM RODA ALFARO <eitham.r.a@hotmail.com>
2023-08-07 20:19:43 -0600

    Modified button about

--- Frontend/ReservationsFrontend/src/pages/about/about.tsx
+++ Frontend/ReservationsFrontend/src/pages/about/about.tsx
@@ -1,12 +1,21 @@
 import { Button } from "@/components/ui/button"
+import { Icons } from "@/assets/icons"
+
 
 export function About (){
+    const [isLoading, setIsLoading] = React.useState<boolean>(false)
     return 
     <>
     <h1> About </h1>
-    <Button>
-        Hola Mundo
-    </Button>
+
+    <Button variant="outline" type="button" disabled={isLoading}>
+        {isLoading ? (
+          <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+        ) : (
+          <Icons.gitHub className="mr-2 h-4 w-4" />
+        )}{" "}
+        Github
+      </Button>
     
     </>
     

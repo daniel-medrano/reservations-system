df1be0fdfdea6ce3ec0d83f6bf736c61ecb0e7e3
EITHAM RODA ALFARO <eitham.r.a@hotmail.com>
2023-08-07 21:07:08 -0600

    Demo payment method

--- Frontend/ReservationsFrontend/src/pages/home/Home.tsx
+++ Frontend/ReservationsFrontend/src/pages/home/Home.tsx
@@ -1,10 +1,12 @@
+import { DemoPaymentMethod } from "@/components/payment-method"
+
 export function Home (){
     return (
     <>
     
     <h1> Home </h1>
-    DemoPaymentMethod()
     
+    <DemoPaymentMethod />
     </>
     
     )

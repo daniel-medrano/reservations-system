9e6d701ea9c20308ee3c489574183b359ab8085e
EITHAM RODA ALFARO <eitham.r.a@hotmail.com>
2023-08-07 20:21:51 -0600

    delted the button

--- Frontend/ReservationsFrontend/src/pages/about/about.tsx
+++ Frontend/ReservationsFrontend/src/pages/about/about.tsx
@@ -1,9 +1,6 @@
-import { Button } from "@/components/ui/button"
-import { Icons } from "@/assets/icons"
-
 
 export function About (){
-    const [isLoading, setIsLoading] = React.useState<boolean>(false)
+  
     return 
     <>
     <h1> About </h1>

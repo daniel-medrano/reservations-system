e02172ab9dd3085fd63d203e828b2378d7bba66c
JOSE DANIEL MEDRANO GUADAMUZ <jose.medranog@uhispano.ac.cr>
2023-08-09 20:35:34 -0600

    Added pagination, sorting, filtering, visibility and row selection to the data table.

--- Frontend/ReservationsFrontend/src/pages/reservations/components/columns.tsx
+++ Frontend/ReservationsFrontend/src/pages/reservations/components/columns.tsx
@@ -1,4 +1,16 @@
 import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import { Checkbox } from "@/components/ui/checkbox"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu"
 
 export type Reservation = {
     id: number
@@ -10,12 +22,34 @@ export type Reservation = {
 
 export const columns: ColumnDef<Reservation>[] = [
     {
+        id: "select",
+        header: ({ table }) => (
+            <Checkbox checked={table.getIsAllPageRowsSelected()} onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)} aria-label="Select all"/>
+        ),
+        cell: ({ row }) => (
+            <Checkbox checked={row.getIsSelected()} onCheckedChange={(value) => row.toggleSelected(!!value)} aria-label="Select row"/>
+        ),
+        enableSorting: false,
+        enableHiding: false
+    },
+    {
         accessorKey: "creationDate",
-        header: "Created at"
+        header: ({ column }) => {
+            return (
+                <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+                    Created at
+                    <ArrowUpDown className="ml-2 h-4 w-4"/>
+                </Button>
+            )
+        }
     },
     {
         accessorKey: "status",
-        header: "Status"
+        header: () => <div className="text-right">Status</div>, // Example
+        cell: ({ row }) => {
+            const formatted = row.getValue("status") ? "Active" : "Inactive"
+            return <div className="text-right font-medium">{formatted}</div>
+        } // Example
     },
     {
         accessorKey: "amountAdults",
@@ -24,5 +58,32 @@ export const columns: ColumnDef<Reservation>[] = [
     {
         accessorKey: "amountChildren",
         header: "Children"
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => {
+            const reservation = row.original
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem onClick={() => navigator.clipboard.writeText(reservation.id.toString())}>
+                            Copy reservation ID
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>View</DropdownMenuItem>
+                        <DropdownMenuItem>Modify</DropdownMenuItem>
+                        <DropdownMenuItem>Delete</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )
+        }
     }
 ]
--- Frontend/ReservationsFrontend/src/pages/reservations/reservations.tsx
+++ Frontend/ReservationsFrontend/src/pages/reservations/reservations.tsx
@@ -9,6 +9,83 @@ import { DataTable } from "./components/data-table"
             amountAdults: 2,
             amountChildren: 2,
             status: true
+        },
+        {
+            id: 2,
+            creationDate: new Date("08-08-2023"),
+            amountAdults: 2,
+            amountChildren: 1,
+            status: true
+        },
+        {
+            id: 3,
+            creationDate: new Date("07-08-2023"),
+            amountAdults: 3,
+            amountChildren: 2,
+            status: true
+        },
+        {
+            id: 4,
+            creationDate: new Date("06-08-2023"),
+            amountAdults: 1,
+            amountChildren: 2,
+            status: true
+        },
+        {
+            id: 5,
+            creationDate: new Date("04-08-2022"),
+            amountAdults: 1,
+            amountChildren: 2,
+            status: true
+        },
+        {
+            id: 6,
+            creationDate: new Date("09-08-2021"),
+            amountAdults: 1,
+            amountChildren: 2,
+            status: true
+        },
+        {
+            id: 1,
+            creationDate: new Date("09-08-2023"),
+            amountAdults: 2,
+            amountChildren: 2,
+            status: true
+        },
+        {
+            id: 2,
+            creationDate: new Date("08-08-2023"),
+            amountAdults: 2,
+            amountChildren: 1,
+            status: true
+        },
+        {
+            id: 3,
+            creationDate: new Date("07-08-2023"),
+            amountAdults: 3,
+            amountChildren: 2,
+            status: true
+        },
+        {
+            id: 4,
+            creationDate: new Date("06-08-2023"),
+            amountAdults: 1,
+            amountChildren: 2,
+            status: true
+        },
+        {
+            id: 5,
+            creationDate: new Date("04-08-2022"),
+            amountAdults: 1,
+            amountChildren: 2,
+            status: true
+        },
+        {
+            id: 6,
+            creationDate: new Date("09-08-2021"),
+            amountAdults: 1,
+            amountChildren: 2,
+            status: true
         }
     ]
 }

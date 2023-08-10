b286920f0f50151c8010464d0bd598e5e9d1393f
JOSE DANIEL MEDRANO GUADAMUZ <jose.medranog@uhispano.ac.cr>
2023-08-10 11:21:27 -0600

    Improved pagination controls.

--- Frontend/ReservationsFrontend/src/pages/reservations/components/data-table.tsx
+++ Frontend/ReservationsFrontend/src/pages/reservations/components/data-table.tsx
@@ -1,4 +1,11 @@
 import {
+    ChevronLeftIcon,
+    ChevronRightIcon,
+    DoubleArrowLeftIcon,
+    DoubleArrowRightIcon,
+} from "@radix-ui/react-icons"
+
+import {
     ColumnDef,
     PaginationState,
     SortingState,
@@ -9,6 +16,7 @@ import {
     getFilteredRowModel,
     getPaginationRowModel,
     getSortedRowModel,
+    getFacetedRowModel,
     useReactTable,
     OnChangeFn
 } from "@tanstack/react-table"
@@ -23,6 +31,13 @@ import {
 } from "@/components/ui/table"
 
 import { Button } from "@/components/ui/button"
+import {
+    Select,
+    SelectTrigger,
+    SelectValue,
+    SelectContent,
+    SelectItem
+} from "@/components/ui/select"
 
 import {
     DropdownMenu,
@@ -37,29 +52,31 @@ import { useState } from "react"
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[]
     data: TData[]
-    pageCount: number
+    totalCount: number
     pagination: PaginationState,
     setPagination: OnChangeFn<PaginationState>
+    sorting: SortingState
+    setSorting: OnChangeFn<SortingState>
 }
 
 export function DataTable<TData, TValue>({
     columns,
     data,
-    pageCount,
+    totalCount,
     pagination,
-    setPagination
+    setPagination,
+    sorting,
+    setSorting
 }: DataTableProps<TData, TValue>) {
-    const [first, setfirst] = useState(0)
-    let {pageIndex, pageSize} = pagination
-    // const [{ pageIndex, pageSize }, setPagination] = useState<PaginationState>({ pageIndex: 1, pageSize: 10 })
-    const [sorting, setSorting] = useState<SortingState>([])
-    console.log(sorting)
+    const { pageIndex, pageSize } = pagination
+    let pageCount = Math.ceil(totalCount / pageSize)
     const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
     console.log(columnFilters)
     const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({})
     console.log(columnVisibility)
     const [rowSelection, setRowSelection] = useState({})
-    console.log(rowSelection)
+    let selectedRowsCount = Object.keys(rowSelection).length
+
     const table = useReactTable({
         data,
         columns,
@@ -68,8 +85,9 @@ export function DataTable<TData, TValue>({
         // getPaginationRowModel: getPaginationRowModel(),
         manualPagination: true,
         pageCount: pageCount,
+        manualSorting: true,
         onSortingChange: setSorting,
-        getSortedRowModel: getSortedRowModel(),
+        // getSortedRowModel: getSortedRowModel(),
         onColumnFiltersChange: setColumnFilters,
         getFilteredRowModel: getFilteredRowModel(),
         onColumnVisibilityChange: setColumnVisibility,
@@ -77,6 +95,7 @@ export function DataTable<TData, TValue>({
         getRowId: (_originalRow, index, _parent) => {
             return (index + pageSize * (pageIndex - 1)).toString()
         },
+        getFacetedRowModel: getFacetedRowModel(),
         state: {
             pagination,
             sorting,
@@ -86,11 +105,10 @@ export function DataTable<TData, TValue>({
         },
         debugTable: true
     })
-
-    console.log(table.getRowModel().rows)
+    console.log(table.getFilteredSelectedRowModel().rows)
+    console.log("Page size: " + pageSize.toString())
     return (
         <div>
-            <button onClick={() => setfirst(first + 1)}>{first}</button>
             <div className="flex items-center py-4">
                 <Input placeholder="Search..." className="max-w-sm" />
                 <DropdownMenu>
@@ -158,17 +176,49 @@ export function DataTable<TData, TValue>({
                     </TableBody>
                 </Table>
             </div>
-            <div className="flex items-center justify-end space-x-2 py-4">
+            <div className="flex items-center justify-between px-2 py-4">
                 <div className="flex-1 text-sm text-muted-foreground">
-                    {table.getFilteredSelectedRowModel().rows.length} of{" "}
-                    {table.getFilteredRowModel().rows.length} row(s) selected.
+                    {selectedRowsCount} of{" "}
+                    {totalCount} row(s) selected.
                 </div>
-                <Button variant="outline" size="sm" onClick={() => setPagination({pageIndex: pageIndex - 1, pageSize})} disabled={pageIndex == 1}>
-                    Previous
-                </Button>
-                <Button variant="outline" size="sm" onClick={() => setPagination({pageIndex: pageIndex + 1, pageSize})} disabled={pageIndex == pageCount}>
-                    Next
-                </Button>
+                <div className="flex items-center space-x-6 lg:space-x-8">
+                    <div className="flex items-center space-x-2">
+                        <p className="text-sm font-medium"> Rows per page</p>
+                        <Select value={pageSize.toString()} onValueChange={(value) => setPagination({ pageIndex: pageIndex, pageSize: Number(value) })}>
+                            <SelectTrigger className="h-8 w-[70px]">
+                                <SelectValue placeholder={pageSize} />
+                            </SelectTrigger>
+                            <SelectContent side="top">
+                                {[10, 20, 30, 40, 50].map((itemPageSize) => (
+                                    <SelectItem key={itemPageSize} value={itemPageSize.toString()}>
+                                        {itemPageSize}
+                                    </SelectItem>
+                                ))}
+                            </SelectContent>
+                        </Select>
+                    </div>
+                    <div className="flex w-[100px] items-center justify-center text-sm font-medium">
+                        Page {pageIndex} of {pageCount}
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <Button variant="outline" size="sm" onClick={() => setPagination({ pageIndex: 1, pageSize })} disabled={pageIndex == 1}>
+                            <span className="sr-only">Go to first page</span>
+                            <DoubleArrowLeftIcon className="h-4 w-4" />
+                        </Button>
+                        <Button variant="outline" size="sm" onClick={() => setPagination({ pageIndex: pageIndex - 1, pageSize })} disabled={pageIndex == 1}>
+                            <span className="sr-only">Go to previous page</span>
+                            <ChevronLeftIcon className="h-4 w-4" />
+                        </Button>
+                        <Button variant="outline" size="sm" onClick={() => setPagination({ pageIndex: pageIndex + 1, pageSize })} disabled={pageIndex == pageCount}>
+                            <span className="sr-only">Go to next page</span>
+                            <ChevronRightIcon className="h-4 w-4" />
+                        </Button>
+                        <Button variant="outline" size="sm" onClick={() => setPagination({ pageIndex: pageCount, pageSize })} disabled={pageIndex == pageCount}>
+                            <span className="sr-only">Go to last page</span>
+                            <DoubleArrowRightIcon className="h-4 w-4" />
+                        </Button>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- Frontend/ReservationsFrontend/src/pages/reservations/reservations.tsx
+++ Frontend/ReservationsFrontend/src/pages/reservations/reservations.tsx
@@ -5,11 +5,12 @@ import { DataTable } from "./components/data-table"
 
 
 import {
-    PaginationState
+    PaginationState,
+    SortingState
 } from "@tanstack/react-table"
 
 
-function getData(pageIndex: number, pageSize: number): Reservation[] {
+function getData(pageIndex: number, pageSize: number, sortBy: string): Reservation[] {
     const reservations = [
         {
             id: 1,
@@ -54,63 +55,82 @@ function getData(pageIndex: number, pageSize: number): Reservation[] {
             status: true
         },
         {
-            id: 1,
+            id: 7,
             creationDate: new Date("09-08-2023"),
             amountAdults: 2,
             amountChildren: 2,
             status: true
         },
         {
-            id: 2,
+            id: 8,
             creationDate: new Date("08-08-2023"),
             amountAdults: 2,
             amountChildren: 1,
             status: true
         },
         {
-            id: 3,
+            id: 9,
             creationDate: new Date("07-08-2023"),
             amountAdults: 3,
             amountChildren: 2,
             status: true
         },
         {
-            id: 4,
+            id: 10,
             creationDate: new Date("06-08-2023"),
             amountAdults: 1,
             amountChildren: 2,
             status: true
         },
         {
-            id: 5,
+            id: 11,
             creationDate: new Date("04-08-2022"),
             amountAdults: 1,
             amountChildren: 2,
             status: true
         },
         {
-            id: 6,
+            id: 12,
             creationDate: new Date("09-08-2021"),
             amountAdults: 1,
             amountChildren: 2,
             status: true
         }
     ]
-
+    switch (sortBy) {
+        case "creationDate":
+            reservations.sort((a, b) => Number(a.creationDate) - Number(b.creationDate))
+            break;
+        case "-creationDate":
+            reservations.sort((a, b) => Number(b.creationDate) - Number(a.creationDate))
+            break;
+        default:
+            reservations.sort((a, b) => Number(a.id) - Number(b.id))
+            break;
+    }
     return reservations.slice(pageIndex * pageSize - pageSize, pageIndex * pageSize)
 }
 
 
 export default function Reservations() {
-    const [{ pageIndex, pageSize }, setPagination] = useState<PaginationState>({ pageIndex: 1, pageSize: 4 })
-    console.log(pageIndex)
-    const data = getData(pageIndex, pageSize)
+    const [{ pageIndex, pageSize }, setPagination] = useState<PaginationState>({ pageIndex: 1, pageSize: 10 })
+    const [sorting, setSorting] = useState<SortingState>([])
+    const sortBy = sorting[0]?.desc ? "-" + sorting[0].id : sorting[0]?.id
+
+    const data = getData(pageIndex, pageSize, sortBy)
 
     return (
         <>
             <div className="container mx-auto py-10"><h1>Reservations</h1></div>
             <div className="container mx-auto">
-                <DataTable columns={columns} data={data} pageCount={3} pagination={{ pageIndex, pageSize }} setPagination={setPagination} />
+                <DataTable
+                    columns={columns}
+                    data={data}
+                    totalCount={12}
+                    pagination={{ pageIndex, pageSize }}
+                    setPagination={setPagination} 
+                    sorting={sorting}
+                    setSorting={setSorting} />
             </div>
 
         </>

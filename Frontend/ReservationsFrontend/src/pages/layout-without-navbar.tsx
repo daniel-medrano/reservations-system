3175ee688a32ceaeaf8872488c53e26935532279
JOSE DANIEL MEDRANO GUADAMUZ <jose.medranog@uhispano.ac.cr>
2023-08-11 12:50:01 -0600

    Enhanced main navbar.

--- Frontend/ReservationsFrontend/src/pages/layout-without-navbar.tsx
+++ Frontend/ReservationsFrontend/src/pages/layout-without-navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavBar } from './navbar';
+import { Nav } from './nav';
 import { Outlet } from 'react-router';
 
 export default function LayoutWithoutNavbar() {

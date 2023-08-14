7c240d65fa6c68774ed1465397bff3499c8938fb
EITHAM RODA ALFARO <eitham.r.a@hotmail.com>
2023-08-14 17:34:16 -0600

    Modified home page

--- Frontend/ReservationsFrontend/src/pages/home/Home.tsx
+++ Frontend/ReservationsFrontend/src/pages/home/Home.tsx
@@ -1,9 +1,68 @@
-
+import roca from "@/assets/roca.jpg"
+import { Icons } from "@/assets/icons"
+import { Button } from "@/components/ui/button"
+import { Link } from 'react-router-dom';
 
 export function Home (){
     return (
     <>
+    <div className="bg-gray-100  flex items-center justify-center">
+        <div className="bg-white p-8 rounded-lg shadow-lg w-full">
+            <div className="pt-4 grid grid-cols-2">
+                <div className="pl-8 col-span-1">
+                    <h1 className="text-3xl font-semibold mb-4">Welcome to Hotel Las Mareas</h1>
+                    <p className="text-gray-600 mb-6">
+                        Experience luxury and comfort in the heart of paradise. In Las Mareas you can experience the gorgeous views of a tropical
+                        country. Relax at the song of the sea with
+                        refreshing beverages, practice sports such as surfing or volleyball and more!
+                    </p>        
+                </div>
+
+                <div className="pl-8 col-span-1 justify-end">
+                    <h2 className="text-3xl font-semibold mb-4 justify-end">Contact us</h2>
+                    <div className="pt-2 grid grid-cols-3">
+                        <div className="pl-2 col-span-1">
+                            <Link to="https://accounts.google.com">    
+                                <Button variant="outline" type="button" >                               
+                                    <Icons.google className="mr-2 h-4 w-4" />
+                                    Google
+                                </Button>
+                            </Link>
+                        </div>
+
+                        <div className="pl-2 col-span-1 justify-end">
+                            <Link to="https://twitter.com/"> 
+                                <Button variant="outline" type="button" >                               
+                                    <Icons.twitter className="mr-2 h-4 w-4" />
+                                    Twitter
+                                </Button>
+                            </Link>
+                        </div>
+
+                        <div className="pl-2 col-span-1 justify-end">
+                            <Link to="https://www.apple.com/">
+                                <Button variant="outline" type="button" >                               
+                                    <Icons.apple className="mr-2 h-4 w-4" />
+                                    Apple
+                                </Button>
+                            </Link>
+                        </div>
+
+                     </div>
+                </div>
+
+            </div>
+            
+           
+        </div>
+    </div>
     
+    <div className="rounded-lg">
+        <img 
+        src= {roca} 
+        className="rounded-md"
+        />
+    </div>
 
     </>
     )

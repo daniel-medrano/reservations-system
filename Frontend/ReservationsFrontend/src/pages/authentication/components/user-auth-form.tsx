c27aaa0d838ad6fddc227af6f065e6370e154b7c
JOSE DANIEL MEDRANO GUADAMUZ <jose.medranog@uhispano.ac.cr>
2023-08-16 11:29:00 -0600

    Change authentication page a little bit.

--- Frontend/ReservationsFrontend/src/pages/authentication/components/user-auth-form.tsx
+++ Frontend/ReservationsFrontend/src/pages/authentication/components/user-auth-form.tsx
@@ -44,7 +44,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
             {isLoading && (
               <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
             )}
-            Log In with Email
+            Sign in
           </Button>
         </div>
       </form>
